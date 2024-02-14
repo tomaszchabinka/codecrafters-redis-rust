27b6b83f5a25af51a647246c75603cb435dfb610
Tomasz Chabinka <tomaszchabinka@gmail.com>
2024-02-14 21:50:03 +0100

    [STEP 3] Respond to multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
 use std::error::Error;
 use std::io::prelude::*;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut _stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    println!("handle_connection");
+    let mut buf = vec![0; 8196];
+    while let Ok(n) = _stream.read(&mut buf) {
+        if n == 0 {
+            break;
+        }
+        //sleep(Duration::from_millis(5 * 1000));
+        println!("received {} bytes", n);
+        println!("data: {:?}", String::from_utf8(Vec::from(&buf[0..n]))?);
+        _ = _stream.write(b"+PONG\r\n");
+    }
+
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                let mut buf = [0; 1024];
-                let n = _stream.read(&mut buf)?;
-                println!("received {} bytes", n);
-                println!("data: {:?}", String::from_utf8(Vec::from(&buf[0..n]))?);
-                _stream.write_all(b"+PONG\r\n")?;
-                _stream.flush()?;
-            }
+            Ok(mut _stream) => _ = handle_connection(_stream),
             Err(e) => {
                 println!("error: {}", e);
             }
