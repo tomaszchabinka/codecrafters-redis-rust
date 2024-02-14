83aec9a313f2e1b36a4403e02bc0e32d19fbf843
Tomasz Chabinka <tomaszchabinka@gmail.com>
2024-02-14 21:42:50 +0100

    [STEP 2] Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-// Uncomment this block to pass the first stage
+use std::error::Error;
+use std::io::prelude::*;
 use std::net::TcpListener;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -11,12 +12,19 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                let mut buf = [0; 1024];
+                let n = _stream.read(&mut buf)?;
+                println!("received {} bytes", n);
+                println!("data: {:?}", String::from_utf8(Vec::from(&buf[0..n]))?);
+                _stream.write_all(b"+PONG\r\n")?;
+                _stream.flush()?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
